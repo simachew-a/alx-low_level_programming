7b8afd1ea2489b5f41f430ba863dff42c464bf80
simachew-a <smith562258@gmail.com>
2023-06-16 00:38:48 -0700

    the c 0x00 program

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fwrite(1.quo.59);
+	return (1);
+}
